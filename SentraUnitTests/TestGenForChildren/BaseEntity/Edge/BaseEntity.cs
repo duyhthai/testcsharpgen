af6a74d31d464471b4bd387dc3093b55ddd45af1
Duy Thai <duy.thai@keytechx.com>
2025-04-08 19:59:16 +0700

    Sentra: Add unit tests

---
+++ SentraUnitTests/TestGenForChildren/BaseEntity/Edge/BaseEntity.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+
+public class BaseEntityTests
+{
+    [Fact]
+    public void DisplayInfo_WithMinValueId_ShouldDisplayCorrectly()
+    {
+        // Arrange
+        var minValueId = default(int);
+        var entity = new DerivedEntity(minValueId);
+
+        // Act
+        entity.DisplayInfo();
+
+        // Assert
+        // Assuming DisplayInfo prints something related to Id
+        Assert.Contains("minValueId", Console.Out.ToString());
+    }
+
+    [Fact]
+    public void DisplayInfo_WithMaxValueId_ShouldDisplayCorrectly()
+    {
+        // Arrange
+        var maxValueId = int.MaxValue;
+        var entity = new DerivedEntity(maxValueId);
+
+        // Act
+        entity.DisplayInfo();
+
+        // Assert
+        // Assuming DisplayInfo prints something related to Id
+        Assert.Contains("maxValueId", Console.Out.ToString());
+    }
+}
+
+public class DerivedEntity : BaseEntity<int>
+{
+    public DerivedEntity(int id) : base(id)
+    {
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Id: {Id}");
+    }
+}
